bfd4f1bdf4420d15eeacb9f8026ef9d8d917249e
Yasin Sutoglu <ysutoglu@gmail.com>
2022-09-14 09:40:26 +0300

    final

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ const results = document.querySelector(".results");
 let interest = 0;
 let payment = 0;
 
-
-
 calcBtn.addEventListener("click" , (e)=>{
     e.preventDefault();
-
     
+     if(selection.value === "" || usance.value === ""  || amount.value ===""){
+        alert("Please fill the areas")
+     }else{
+         if (selection.value === "house-loan") {
+           interest = 0.89 ;
+         } else if (selection.value === "consumer.loan") {
+           interest = 1.19;
+         } else if (selection.value === "auto-loan") {
+           interest = 1.09;
+         }
 
-    if(selection.value === "house-loan"){
-        interest = 0.89 / 100;
-    }else if(selection.value === "consumer.loan"){
-        interest = 1.19 /100;
-    }else if(selection.value === "auto-loan"){
-        interest = 1.09 /100;
-    }
+         payment =
+           (+amount.value * ((interest/100) * (1 + interest/100) ** +usance.value)) /
+           ((1 + interest/100) ** +usance.value - 1);
 
-    payment = (amount.value) * (interest*((1+interest)**usance.value))/(((1+interest)**usance.value)-1)
-
-
-    results.innerHTML = `
+         results.innerHTML = `
         <h2 class="mt-3 text-info">Credit Informations</h2>
         <table class="table table-bordered border-info  mt-4">
             <tbody>
@@ -43,15 +43,16 @@ calcBtn.addEventListener("click" , (e)=>{
                 <td>${interest}</td>
                 </tr>
                 <tr>
-                <th>Total</th>
-                <td>${(+payment*usance.value).toFixed(2)}$</td>
+                <th>Total Payment</th>
+                <td>${Number((payment * +usance.value).toFixed(2))}$</td>
                 <th>Monthly Payment</th>
                 <td>${+payment.toFixed(2)}$</td>
                 </tr>
             </tbody>
         </table>
     `;
-
+   }  
+   
 });
 
 
